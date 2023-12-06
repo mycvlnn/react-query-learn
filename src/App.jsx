6e3edbeb965dcc6929a65b04dd44e37198d6358b
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-12-06 11:04:17 +0700

    feat: react-query vs react-router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
 import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
+import EditEvent, {
+    loader as editEventLoader,
+    action as editEventAction,
+} from "./components/Events/EditEvent.jsx";
 import { queryClient } from "./utils/http.js";
 
 const router = createBrowserRouter([
@@ -30,6 +33,8 @@ const router = createBrowserRouter([
             {
                 path: "/events/:id/edit",
                 element: <EditEvent />,
+                loader: editEventLoader,
+                action: editEventAction,
             },
         ],
     },
--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,62 +1,61 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { Link, redirect, useNavigate, useNavigation, useParams, useSubmit } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { fetchEvent, queryClient, updateEvent } from "../../utils/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
-import ErrorBlock from "../UI/ErrorBlock.jsx";
+// import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+// import ErrorBlock from "../UI/ErrorBlock.jsx";
 import { QUERY_KEY } from "../../constants/queryKey.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
     const navigate = useNavigate();
     const params = useParams();
+    const submit = useSubmit();
+    const { state } = useNavigation();
 
-    const { data, isPending, isError, error } = useQuery({
+    const { data, isError, error } = useQuery({
         queryKey: [QUERY_KEY.EVENTS, { id: params.id }],
         queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+        staleTime: 10000,
     });
 
-    const { mutate } = useMutation({
-        mutationFn: updateEvent,
-        onMutate: async (payload) => {
-            // payload là dữ liệu nhận được từ hàm mutate trả ra
-            // On mutate, so this function here, will be executed right when you call mutate.
-            // update the data that's cached by React Query
+    // const { mutate } = useMutation({
+    //     mutationFn: updateEvent,
+    //     onMutate: async (payload) => {
+    //         // payload là dữ liệu nhận được từ hàm mutate trả ra
+    //         // On mutate, so this function here, will be executed right when you call mutate.
+    //         // update the data that's cached by React Query
 
-            const newEvent = payload.event;
+    //         const newEvent = payload.event;
 
-            // to cancel all active queries for a specific key
-            await queryClient.cancelQueries({ queryKey: [QUERY_KEY.EVENTS, { id: params.id }] });
+    //         // to cancel all active queries for a specific key
+    //         await queryClient.cancelQueries({ queryKey: [QUERY_KEY.EVENTS, { id: params.id }] });
 
-            // get previous event (rollback if error occurred)
-            const previousEvent = queryClient.getQueryData([QUERY_KEY.EVENTS, { id: params.id }]);
+    //         // get previous event (rollback if error occurred)
+    //         const previousEvent = queryClient.getQueryData([QUERY_KEY.EVENTS, { id: params.id }]);
 
-            queryClient.setQueryData([QUERY_KEY.EVENTS, { id: params.id }], newEvent);
+    //         queryClient.setQueryData([QUERY_KEY.EVENTS, { id: params.id }], newEvent);
 
-            // trả về để hàm error có thể xử lý rollback
-            return {
-                previousEvent,
-            };
-        },
-        onError: (error, data, context) => {
-            // rolling back
-            queryClient.setQueryData([QUERY_KEY.EVENTS, { id: params.id }], context.previousEvent);
-        },
-        onSettled: () => {
-            // Xử lý đồng bộ dữ liệu từ phía client và server
-            queryClient.invalidateQueries([QUERY_KEY.EVENTS, { id: params.id }]);
-        },
-    });
+    //         // trả về để hàm error có thể xử lý rollback
+    //         return {
+    //             previousEvent,
+    //         };
+    //     },
+    //     onError: (error, data, context) => {
+    //         // rolling back
+    //         queryClient.setQueryData([QUERY_KEY.EVENTS, { id: params.id }], context.previousEvent);
+    //     },
+    //     onSettled: () => {
+    //         // Xử lý đồng bộ dữ liệu từ phía client và server
+    //         queryClient.invalidateQueries([QUERY_KEY.EVENTS, { id: params.id }]);
+    //     },
+    // });
 
     // Hàm xử lý submit form
     function handleSubmit(formData) {
-        mutate({
-            id: params.id,
-            event: formData,
-        });
-
-        navigate("../");
+        submit(formData, { method: "PUT" });
     }
 
     function handleClose() {
@@ -65,13 +64,13 @@ export default function EditEvent() {
 
     let content;
 
-    if (isPending) {
-        content = (
-            <div className="center">
-                <LoadingIndicator />
-            </div>
-        );
-    }
+    // if (isPending) {
+    //     content = (
+    //         <div className="center">
+    //             <LoadingIndicator />
+    //         </div>
+    //     );
+    // }
 
     if (isError) {
         content = (
@@ -89,15 +88,40 @@ export default function EditEvent() {
     if (data) {
         content = (
             <EventForm inputData={data} onSubmit={handleSubmit}>
-                <Link to="../" className="button-text">
-                    Cancel
-                </Link>
-                <button type="submit" className="button">
-                    Update
-                </button>
+                {state === "submitting" ? (
+                    <button className="button">Sending data...</button>
+                ) : (
+                    <>
+                        <Link to="../" className="button-text">
+                            Cancel
+                        </Link>
+                        <button type="submit" className="button">
+                            Update
+                        </button>
+                    </>
+                )}
             </EventForm>
         );
     }
 
     return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+    // Thay vì sử dụng hook ta có thể sử dụng hàm này thủ công
+    return queryClient.fetchQuery({
+        queryKey: [QUERY_KEY.EVENTS, { id: params.id }],
+        queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    });
+}
+
+export async function action({ request, params }) {
+    const formData = await request.formData();
+
+    const updatedEventData = Object.fromEntries(formData);
+
+    await updateEvent({ id: params.id, event: updatedEventData });
+    await queryClient.invalidateQueries({ queryKey: [QUERY_KEY.EVENTS] });
+
+    return redirect("../");
 }
